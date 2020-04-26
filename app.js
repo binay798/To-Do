578beadaa3b2202a595f9f6f5e9b3c77e13080c0
binay6014 <binayshrestha798@gmail.com>
2020-04-26 10:56:12 +0545

    Initialized

--- app.js
+++ app.js
@@ -1,63 +1,91 @@
-document.querySelector(".fa-plus-circle").addEventListener("click",()=>{
-    clickEvent();
-    deleteItem();
-    
-})
-document.addEventListener("keypress",function(event){
-    if(event.keyCode == 13){
-        clickEvent();
-        deleteItem();
-        
-    }
-    
-})
-
-function clickEvent(){
-    let inputField = document.querySelector(".add-to-do");
-    if(inputField.value !== ""){
-        let text = "<div class=\"item\"><p class=\"para\"><i class=\"fa fa-check-circle\" style=\"font-size: 20px;margin-right: 5px;color: aqua;\"></i><span class=\"info-detail\">$Exercise</span><span><i class=\"fa fa-times-circle\" style=\"float:right;font-size: 20px;color:red;\"></i></span></p></div>";
-        let newText = text.replace("$Exercise",inputField.value);
-        document.querySelector(".items").insertAdjacentHTML("beforeend",newText);
-    }
-    inputField.value = "";
-   
+const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+const months = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'];
+//Creating the function constructor of Person
+function Person(firstname,lastname,email,info){
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.info = info;
 }
-function deleteItem(){
-    let items = document.querySelectorAll(".fa-times-circle");
-    items.forEach(item =>{
-        item.addEventListener("click",(event) =>{
-            event.target.parentNode.parentNode.parentNode.remove();
-        });
-        item.addEventListener("mouseover",(event) =>{
-            event.target.style.color = 'brown';
-        })
-        item.addEventListener("mouseout",(event) =>{
-            event.target.style.color = 'red';
-        })
-    })
-    
 
-    let tick = document.querySelectorAll(".fa-check-circle");
-    tick.forEach(item =>{
-        item.addEventListener("click",(e) =>{
-            let color = e.target.style.color;
-            
-            if(color == 'aqua'){
-                e.target.style.color = 'brown';
-                e.target.nextSibling.style.color = 'gray';
-                e.target.nextSibling.style.fontStyle = 'italic';
-                e.target.nextSibling.style.textDecoration = 'line-through';
-            }else{
-                e.target.style.color = 'aqua';
-                e.target.nextSibling.style.color = 'black';
-                e.target.nextSibling.style.fontStyle = 'normal';
-                e.target.nextSibling.style.textDecoration = 'none';
+
+//Gettin all info from user input
+function getAllInfo(){
+    let firstName = document.querySelector(".firstname").value;
+    let lastName = document.querySelector(".lastname").value;
+    let email = document.querySelector(".email").value;
+    let info = document.querySelector(".your-info").value;
+
+    firstName = new Person(firstName,lastName,email,info);
+    
+    return {
+        info:firstName,
+    }
+}
+
+
+//Adding items to local storage
+function addAllItemsToLocal(){
+    let info = getAllInfo().info;
+    if(info.firstname !== "" || info.lastname !== ""){
+        let getItemFromLocal = localStorage.getItem("data");
+        getItemFromLocal = JSON.parse(getItemFromLocal);
+        if(getItemFromLocal !== null){
+            getItemFromLocal = {
+                ...getItemFromLocal,
+                [info.firstname]:info,
             }
-        })
-    })
+        }else{
+            getItemFromLocal = {
+                [info.firstname]:info,
+            };
+        }
+        
+        localStorage.setItem("data",JSON.stringify(getItemFromLocal));
+    }
+    location.reload();
+    
+}
 
+function displayInfo(){
+    //Getting items from local storage
+    let allInfo = localStorage.getItem("data");
+    //parsing json to js object
+    allInfo = JSON.parse(allInfo);
+    //creating a list of objects
+    listOfAllInfo = Object.values(allInfo);
+    
+    for(let i=listOfAllInfo.length-1;i>=0;i--){
+        let date = new Date();
+        let html = `<div class="all-infos"><div class="info"><div \
+                class="left"><img src="images/profile.png" alt="" class="profile"">\
+                </div><div class="right"><p><b>Author:</b><br><span class="firstname">${listOfAllInfo[i].firstname}</span> ${listOfAllInfo[i].lastname} </p><p>${listOfAllInfo[i].email}\
+                </p><h3 style="color:rgb(32, 25, 25);margin-top:20px;">Your Details:</h3>\
+                <p style="color:rgb(39, 36, 36)">${listOfAllInfo[i].info}</p>
+                
+                </div><i class="fa fa-trash-o fa-2x"></i></div></div>`;
+
+        let x = document.querySelector(".infos");
+        x.innerHTML += html;
+        resetInputField();
+    }
+        
     
     
+    
+    
+}
+displayInfo();
+
+
+
+
+
+function resetInputField(){
+    document.querySelector(".firstname").value = "";
+    document.querySelector(".lastname").value = "";
+    document.querySelector(".email").value = "";
+    document.querySelector(".your-info").value = "";
 }
 
 
@@ -67,17 +95,18 @@ function deleteItem(){
 
 
 
-/*document.querySelector(".fa-check-circle").addEventListener("click",(event)=>{
-    if(event.target.style.color == 'aqua'){
-        event.target.style.color = 'brown';
-        document.querySelector(".info-detail").style.textDecoration = 'line-through';
-        document.querySelector(".info-detail").style.color = 'gray';
-        document.querySelector(".info-detail").style.fontStyle = 'italic';
-    }else{
-        event.target.style.color = 'aqua';
-        document.querySelector(".info-detail").style.textDecoration = 'none';
-        document.querySelector(".info-detail").style.color = 'black';
-        document.querySelector(".info-detail").style.fontStyle = 'normal';
+
+/*
+if(allInfo.firstname !=="" || allInfo.lastname !== ""){
+        let html = `<div class="all-infos"><div class="info"><div \
+                class="left"><img src="images/profile.png" alt="" class="profile"">\
+                </div><div class="right"><p><b>Author:</b><br>${allInfo.firstname} ${allInfo.lastname} </p><p>${allInfo.email}\
+                </p><h3 style="color:rgb(32, 25, 25);margin-top:20px;">Your Details:</h3>\
+                <p style="color:rgb(39, 36, 36)">${allInfo.info}</p><p>Published at:</p>
+                <p class="date" style="color:gray;"></p></div></div></div>`;
+
+        addAllItemsToLocal(allInfo);
+        resetInputField();
     }
-})
+
 */
